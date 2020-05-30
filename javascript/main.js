4880824bd500aba730a198d72ec8d6760793ed7b
Pebanh <peter.banh95@gmail.com>
2020-05-30 13:26:27 -0400

    Add functionality to ensure player does not lose on first turn. This involves rearranging the gameGrid before the person clicks. Also add testing function to ensure game grid values makes sense

--- javascript/main.js
+++ javascript/main.js
@@ -1,10 +1,11 @@
-// Initialize State and Constants
+/* -------- Initialize State and Constants -------- */
 var gameGridDOM = document.getElementById("game-grid");
 var gameTimerDOM = document.querySelector("#timer p");
 var gameMineDOM = document.querySelector("#num-of-mines");
 var gameResetDOM = document.querySelector(".reset");
 var gameMenuDOM = document.querySelector("#settings ul");
 var musicDOM = document.querySelector("#music");
+
 var coinAudio = document.querySelector("#coinAudio");
 var themeAudio = document.querySelector("#themeAudio");
 var gameOverAudio = document.querySelector("#gameOverAudio");
@@ -42,6 +43,7 @@ var gameDiffSet = {
     numOfMines: 99,
   },
 };
+
 var gameDiff = "Normal";
 var gridX;
 var gridY;
@@ -54,56 +56,60 @@ var gameGridReveal = [];
 var gameStatus = 0;
 var soundSetting = 0;
 var explosionSet = 0;
-var resized = 0;
 
-// Update State Function
-function floodFill(square, x, y) {
-  rerenderSquare(square, x, y);
+/* -------- Utility Functions -------- */
+
+function checkSurroundings(y, x, fn) {
   for (let dx = -1; dx <= 1; dx++) {
     if (x + dx >= 0 && x + dx < gridX) {
       for (let dy = -1; dy <= 1; dy++) {
         if (y + dy >= 0 && y + dy < gridY) {
-          if (gameGridReveal[y + dy][x + dx] == 0) {
-            if (gameGridValues[y + dy][x + dx] > 0) {
-              let checkedSquare = document.getElementById(
-                `${y + dy},${x + dx}`
-              );
-              rerenderSquare(checkedSquare, x + dx, y + dy);
-            } else {
-              let checkedSquare = document.getElementById(
-                `${y + dy},${x + dx}`
-              );
-              setTimeout(function () {
-                floodFill(checkedSquare, x + dx, y + dy);
-              }, 300 * Math.random());
-            }
-          }
+          fn(y + dy, x + dx);
         }
       }
     }
   }
 }
 
-function mineNumGen() {
-  for (let minesAdded = 0; minesAdded < numOfMines; minesAdded++) {
+/* -------- Update State Functions -------- */
+function floodFill(square, x, y) {
+  rerenderSquare(square, x, y);
+
+  checkSurroundings(y, x, function (y2, x2) {
+    if (gameGridReveal[y2][x2] == 0) {
+      if (gameGridValues[y2][x2] > 0) {
+        let checkedSquare = document.getElementById(`${y2},${x2}`);
+        rerenderSquare(checkedSquare, x2, y2);
+      } else {
+        let checkedSquare = document.getElementById(`${y2},${x2}`);
+        setTimeout(function () {
+          floodFill(checkedSquare, x2, y2);
+        }, 300 * Math.random());
+      }
+    }
+  });
+}
+
+function mineNumGen(totalMines = numOfMines, y = 99, x = 99) {
+  for (let minesAdded = 0; minesAdded < totalMines; minesAdded++) {
     randX = Math.floor(Math.random() * gridX);
     randY = Math.floor(Math.random() * gridY);
-    while (gameGridValues[randY][randX] == -1) {
+    while (
+      gameGridValues[randY][randX] == -1 ||
+      Math.abs(randY - y) <= 1 ||
+      Math.abs(randX - x) <= 1
+    ) {
       randX = Math.floor(Math.random() * gridX);
       randY = Math.floor(Math.random() * gridY);
     }
-    gameGridValues[randY][randX] = -1;
-    for (let dx = -1; dx <= 1; dx++) {
-      if (randX + dx >= 0 && randX + dx < gridX) {
-        for (let dy = -1; dy <= 1; dy++) {
-          if (randY + dy >= 0 && randY + dy < gridY) {
-            if (gameGridValues[randY + dy][randX + dx] != -1) {
-              gameGridValues[randY + dy][randX + dx]++;
-            }
-          }
-        }
-      }
+    if (totalMines == 1) {
     }
+    gameGridValues[randY][randX] = -1;
+    checkSurroundings(randY, randX, function (y, x) {
+      if (gameGridValues[y][x] != -1) {
+        gameGridValues[y][x]++;
+      }
+    });
     gameGridReveal[randY][randX] = 1;
   }
 }
@@ -112,7 +118,7 @@ function calcRemainSq() {
   remainingSq = gridX * gridY - numOfMines;
 }
 
-// Render Functions
+/* -------- Render Functions -------- */
 function renderGameGrid() {
   for (y = 0; y < gridY; y++) {
     let valueRow = [];
@@ -128,6 +134,7 @@ function renderGameGrid() {
       square.id = y + "," + x;
       gameGridDOM.appendChild(square);
     }
+
     gameGridValues.push(valueRow);
     gameGridReveal.push(revealRow);
   }
@@ -137,6 +144,7 @@ function rerenderSquare(square, squareX, squareY) {
   if (square.classList.value.includes("open")) {
     return;
   }
+
   coinAudio.pause();
   coinAudio.currentTime = 0;
   coinAudio.play();
@@ -151,12 +159,16 @@ function rerenderSquare(square, squareX, squareY) {
 }
 
 function renderWinLoss() {
+  clearInterval(timer);
+
   explosionSet = 1;
+  gameStatus = 1;
+  let mineCount = 0;
+
+  let mario = document.createElement("div");
   let popup = document.createElement("div");
   popup.setAttribute("id", "popup");
-  clearInterval(timer);
-  let square;
-  let mineCount = 0;
+
   gameGridValues.forEach(function (row, y) {
     row.forEach(function (squareValue, x) {
       if (squareValue == -1) {
@@ -171,7 +183,7 @@ function renderWinLoss() {
       }
     });
   });
-  let mario = document.createElement("div");
+
   if (remainingSq > 0) {
     setTimeout(function () {
       themeAudio.pause();
@@ -207,7 +219,6 @@ function renderWinLoss() {
       }, 3000);
     }, 2300);
   }
-  gameStatus = 1;
 }
 
 function renderTimer() {
@@ -218,20 +229,42 @@ function renderMines() {
   gameMineDOM.innerText = numOfMines + " Mines";
 }
 
-// Event Handler Functions
+/* -------- Event Handler Functions -------- */
 function squareClick(event) {
-  if (timeElap === null) {
-    timeElap = 0;
-    timer = setInterval(renderTimer, 1000);
-  }
-  // Checking the event
+  // Set event target as square
   let square = event.target;
+  // Return if it is not a valid square
   if (!square.classList.value.includes("square") || gameStatus) {
     return;
   }
-  // Extracting information from event
+
+  // Find coordinates of clicked square
   let squareX = parseInt(square.id.split(",")[1]);
   let squareY = parseInt(square.id.split(",")[0]);
+
+  // Start timer if not yet started and then reorganize grid. This is to
+  // ensure the player doesn't lose right away.
+  if (timeElap === null) {
+    timeElap = 0;
+    timer = setInterval(renderTimer, 1000);
+    checkSurroundings(squareY, squareX, function (y, x) {
+      if (gameGridValues[y][x] == -1) {
+        mineNumGen(1, y, x);
+        gameGridValues[y][x] = 0;
+        gameGridReveal[y][x] = 0;
+        checkSurroundings(y, x, function (y2, x2) {
+          if (gameGridValues[y2][x2] > 0) {
+            if (!(y2 == y && x2 == x)) {
+              gameGridValues[y2][x2]--;
+            }
+          }
+          if (gameGridValues[y2][x2] == -1) {
+            gameGridValues[y][x]++;
+          }
+        });
+      }
+    });
+  }
 
   if (gameGridValues[squareY][squareX] == -1) {
     gameStatus = 1;
@@ -367,23 +400,62 @@ function respond() {
   }
 }
 
-//Initialize Functions
+/* -------- Initialization Functions -------- */
 function initialize() {
   gridX = gameDiffSet[gameDiff]["gridX"];
   gridY = gameDiffSet[gameDiff]["gridY"];
   numOfMines = gameDiffSet[gameDiff]["numOfMines"];
   gameGridDOM.style.width =
     gridX * gameDiffSet[gameDiff]["squareSize"] + 4 + "px";
+
   renderGameGrid();
   renderMines();
   calcRemainSq();
   mineNumGen();
+  respond();
+
   gameGridDOM.onclick = squareClick;
   gameResetDOM.onclick = reset;
   gameMenuDOM.onclick = selectDiff;
   musicDOM.onclick = musicToggle;
   window.addEventListener("resize", respond);
-  respond();
 }
 
 initialize();
+
+/* -------- Testing Function for Development -------- */
+function testValues(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let count;
+    for (let j = 0; j < arr[0].length; j++) {
+      if (arr[i][j] > 0) {
+        count = 0;
+        for (let di = -1; di <= 1; di++) {
+          if (i + di >= 0 && i + di < arr.length) {
+            for (let dj = -1; dj <= 1; dj++) {
+              if (j + dj >= 0 && j + dj < arr[0].length) {
+                if (arr[i + di][j + dj] == -1) {
+                  count++;
+                }
+              }
+            }
+          }
+        }
+        if (count != arr[i][j]) return false;
+      } else if (arr[i][j] == -1) {
+        for (let di = -1; di <= 1; di++) {
+          if (i + di >= 0 && i + di < arr.length) {
+            for (let dj = -1; dj <= 1; dj++) {
+              if (j + dj >= 0 && j + dj < arr[0].length) {
+                if (arr[i + di][j + dj] == 0) {
+                  return false;
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  return true;
+}
